b207c7e225a7615edd09627b8c8183462d779974
Sandesh Sarade <Saradesandesh@gmail.com>
2022-12-05 16:53:45 +0530

    change in if condition || into &&

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ function time() {
   var ht = dt.getHours();
   let mt = dt.getMinutes();
   let st = dt.getSeconds();
-  let format = document.getElementById("session");
+  let format = "AM"
 
   document.getElementById("hrs").innerHTML = ht;
   document.getElementById("min").innerHTML = mt;
@@ -28,41 +28,71 @@ function time() {
     session.innerHTML = "AM";
   }
 
-  if (ht >= 7 || ht < 11) {
-    var change = document.getElementById("dowork");
+  if (ht >= 05 && ht < 11) {
+    var change = document.getElementById("behave");
     change.innerHTML = `GRAB SOME HEALTHY BREKFAST!!`;
-    var goodmorning = document.getElementById("gn");
-    goodmorning.innerHTML = "GOOD MORNING!! WAKE UP!!";
-    var img=document.getElementById('myimg');
+    var goodnight = document.getElementById("vibes");
+    goodnight.innerHTML = "GOOD MORNING!! WAKE UP!!";
+    var img=document.getElementById('images');
     img.src='img/morning.png';
   }
-  if (ht >= 11 || ht < 15) {
-    var chintu = document.getElementById("dowork");
-    chintu.innerHTML = `LET'S HAVE SOME LUNCH !!`;
-    var goodafternoon = document.getElementById("gn");
-    goodafternoon.innerHTML = "GOOD AFTERNOON !! TAKE SOME SLEEP";
-    var img1=document.getElementById('myimg');
-   img1.src='img/afterlunch.png';
+   if (ht >= 11 && ht <15 ) {
+    var change = document.getElementById("behave");
+    change.innerHTML = `GOOD AFTERNOON !! TAKE SOME SLEEP`;
+    var goodnight = document.getElementById("vibes");
+    goodnight.innerHTML = " LET'S HAVE SOME LUNCH !";
+    var img=document.getElementById('images');
+    img.src='img/afterlunch.png';
   }
-
-  if (ht >= 15 || ht < 20) {
-    var person = document.getElementById("dowork");
-    person.innerHTML = "STOP YAWINING";
-    var goodevening = document.getElementById("gn");
-    goodevening.innerHTML = "GOOD EVENING!!  ";
-   var img2=document.getElementById('myimg');
-   img2.src='img/evening.png';
+ if (ht >= 15 && ht < 20) {
+    var change = document.getElementById("behave");
+    change.innerHTML = `GOOD EVENING!!`;
+    var goodnight = document.getElementById("vibes");
+    goodnight.innerHTML = "STOP YAWINING";
+    var img=document.getElementById('images');
+    img.src='img/evening.png';
   }
-  if (ht >= 20 || ht < 7) {
-    var newchange = document.getElementById("dowork");
-    newchange.innerHTML = "Close Your Eyes and Go to Sleep!!";
-    var goodnight = document.getElementById("gn");
-    goodnight.innerHTML = "GOOD NIGHT!!";
-    var img3=document.getElementById('myimg');
-    img3.src='img/goodnight.png';
+ if (ht >= 20 && ht < 24 ) {
+    var change = document.getElementById("behave");
+    change.innerHTML = `GOOD NIGHT!!`;
+    var goodnight = document.getElementById("vibes");
+    goodnight.innerHTML = "Close Your Eyes and Go to Sleep!!";
+    var img=document.getElementById('images');
+    img.src='img/goodnight.png';
   }
-
 }
 setInterval(time, 1000);
 
+function changeEvent(){
+  let dt = new Date();
+  var ht = dt.getHours();
+  let mt = dt.getMinutes();
+  let st = dt.getSeconds();
+  let format = "AM"
 
+  document.getElementById("hrs").innerHTML = ht;
+  document.getElementById("min").innerHTML = mt;
+  document.getElementById("sec").innerHTML = st;
+  
+  if(ht >= 04 || ht < 11){
+    var img=document.getElementById('images');
+    img.src='img/morning.png';
+  }
+  else if(ht >= 12 || ht < 15){
+    var img=document.getElementById('images');
+    img.src='img/afterlunch.png';
+  }
+  if(ht >= 16 || ht < 20){
+    var img=document.getElementById('images');
+    img.src='img/evening.png';
+  }
+  if(ht >= 20 || ht < 24){
+    var img=document.getElementById('images');
+    img.src='img/goodnight.png';
+  }
+   
+
+
+
+}
+setTimeout(changeEvent,500000)
